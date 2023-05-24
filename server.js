1d2ebd717baf15dd68f9d5bde806d0f93ba78415
mfrascarolo <frascarolomartina@gmail.com>
2023-05-24 17:55:36 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+/*
+1) Instalar: 
+            npm i express
+            npm i cors path dotenv
+            npm install -g nodemon
+            npm i uid
+            npm i mongoose
+            npm i jsonwebtoken
+2) Para levantar el server: node server.js, con nodemon: nodemon server.js
+3) 
+req -> para leer informacion 
+res -> para enviar informacion
+4) JSON formater para mejorar la vista de los objetos en google (extension)
+5) req.params -> para leer informacion que viene por parametro en la url
+6) Instalar postman
+7) Falta vincular username con pedidos y getpedidos con username para que traiga el pedido de ese usuario con el token
+*/
